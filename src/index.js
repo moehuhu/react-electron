86fb7208863eeb0ec19a532c59a11b8c6e41d684
moehuhu <994458697@qq.com>
2021-05-03 20:38:09 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import App from './App';
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
